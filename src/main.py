1f192bbe7776a1f08a828a1af1defc196dde6600
Dmitryi <ilacker106@gmail.com>
2024-02-26 09:17:31 +0700

    DZ_13.4 <сделал Директорию python пакетом, чтобы импорты работали правильно,>

--- src/main.py
+++ src/main.py
@@ -20,17 +20,17 @@ def main():
 
     product_one_instance = Product.create_product(prod_one_data)
     product_two_instance = Product.create_product(prod_two_data)
-    # print(type(product_one_instance))
-    # print(product_one_instance)
+    print(type(product_one_instance))
+    print(product_one_instance)
     all_products = [product_two_instance, product_one_instance]
     category = Category('Смартфоны', 'Категория для смартфонов', all_products)
     print(category)
-    # print(Product.prices)
-    # print(product_one_instance + product_two_instance)
-    # print(category.products)
-    # print(Product.create_product(prod_two_data))
-    # print(category.total_categories)
-    # print(category.total_products)
+    print(Product.price)
+    print(product_one_instance + product_two_instance)
+    print(category.products)
+    print(Product.create_product(prod_two_data))
+    print(category.total_categories)
+    print(category.total_products)
 
 
 if __name__ == '__main__':
